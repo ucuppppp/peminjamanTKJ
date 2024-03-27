b0429f158db8d16792c3a8f9dfb2cc8a0c92e45b
Your Name <you@example.com>
2024-03-27 10:34:14 +0700

    manttap

---
+++ resources/js/Pages/Posts/Testing.jsx
@@ -0,0 +1,9 @@
+import react from "react";  
+
+export default function Testing(){
+    return (
+        <>
+        <h1 className="text-5xl text-blue-500 font-bold text-center">TESTINGG BROWWWW</h1>
+        </>
+    )
+}
